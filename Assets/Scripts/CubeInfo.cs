42b7b38e5a1052adcfb75f82c3f7129a9d59b8b3
sinword <samwang9199@gmail.com>
2023-12-11 02:31:02 +0800

    JSON record position completed

---
+++ Assets/Scripts/CubeInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class CubeInfo {
+    public string cubeName;
+    public Vector3 position;
+}
